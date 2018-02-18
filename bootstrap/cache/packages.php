dcd7813ab624533b66bc5fb39cac75f2223e7942
remindbg <remindbg@gmail.com>
2018-02-19 00:58:09 +0200

    Added files from linux

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,27 @@
+<?php return array (
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'barryvdh/laravel-debugbar' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\Debugbar\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Debugbar' => 'Barryvdh\\Debugbar\\Facade',
+    ),
+  ),
+);
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,11 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('city')->nullable();
+            $table->integer('age')->nullable();
+            $table->boolean('isAdmin')->default(false);
+            $table->boolean('isAuthor')->default(false);
+            // todo images, etc
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
